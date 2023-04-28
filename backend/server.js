52a06482d2f496307312f2f6a28cac5a8be7fa6f
jonah-angelo <jonah.angelo@gmail.com>
2023-04-28 13:54:22 -0700

    please work

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@
 require('dotenv').config()
 const express = require('express');
 const path = require('path')
+const cors = require('cors')
 
 
 /* Require the db connection, models, and seed data
@@ -24,6 +25,8 @@ const app = express();
 
 /* Middleware (app.use)
 --------------------------------------------------------------- */
+// CORS middleware:
+app.use(cors())
 // body parser - used for POST/PUT/PATCH routes:
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json())
@@ -31,10 +34,6 @@ app.use(express.json())
 // use the React build folder for static files
 app.use(express.static(path.join(path.dirname(__dirname), 'frontend', 'dist')))
 
-// Any other route not matching the routes above gets routed by React
-app.get('*', (req, res) => {
-    res.sendFile(path.join(path.dirname(__dirname), 'frontend', 'dist', 'index.html'));
-});
 
 
 /* Mount routes
@@ -48,18 +47,23 @@ app.use('/api/exercises', exercisesCtrl)
 app.get('/seed', function (req, res) {
     // Remove any existing pets
     db.Exercise.deleteMany({})
-        .then(removedExercises => {
-            console.log(`Removed ${removedExercises.deletedCount} exercises`)
-
-            // Seed the pets collection with the seed data
-            db.Exercise.insertMany(db.seedExercises)
-                .then(addedExercises => {
-                    console.log(`Added ${addedExercises.length} exercises`)
-                    res.json(addedExercises)
-                })
+    .then(removedExercises => {
+        console.log(`Removed ${removedExercises.deletedCount} exercises`)
+        
+        // Seed the pets collection with the seed data
+        db.Exercise.insertMany(db.seedExercises)
+        .then(addedExercises => {
+            console.log(`Added ${addedExercises.length} exercises`)
+            res.json(addedExercises)
         })
+    })
 });
 
+
+// Any other route not matching the routes above gets routed by React
+app.get('*', (req, res) => {
+    res.sendFile(path.join(path.dirname(__dirname), 'frontend', 'dist', 'index.html'));
+});
 
 /* Tell the app to listen on the specified port
 --------------------------------------------------------------- */
