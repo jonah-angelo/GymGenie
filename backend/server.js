c3451b54f1afd9bb3300dcf5ff49e98e1340e74f
jonah-angelo <jonah.angelo@gmail.com>
2023-04-28 13:47:46 -0700

    bug

--- backend/server.js
+++ backend/server.js
@@ -29,7 +29,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json())
 
 // use the React build folder for static files
-app.use(express.static(path.join(path.dirname(__dirname), 'frontend', 'dist', 'index.html')))
+app.use(express.static(path.join(path.dirname(__dirname), 'frontend', 'dist')))
 
 // Any other route not matching the routes above gets routed by React
 app.get('*', (req, res) => {
